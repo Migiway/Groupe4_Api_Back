b9f31eb012bf99e7e0e79d7f53d18ddd6b7898d4
thomas <thomas.martin6070@gmail.com>
2019-01-07 20:02:08 +0100

    ajout du form nb salary

--- src/Entity/NbSalary.php
+++ src/Entity/NbSalary.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\NbSalaryRepository")
@@ -20,11 +21,13 @@ class NbSalary
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
      */
     private $salary_libelle;
 
     /**
      * @ORM\OneToMany(targetEntity="App\Entity\Parameter", mappedBy="nbSalary")
+     * @Assert\NotBlank
      */
     private $parameter_id;
 
