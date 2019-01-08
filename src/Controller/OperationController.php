9c5176fdaa076c03ade1bf738125a8db9104c23e
SofianeAguero <azi.sofiane@hotmail.fr>
2019-01-08 10:11:37 +0100

    modification des forms type

--- src/Controller/OperationController.php
+++ src/Controller/OperationController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Controller;
-
 use App\Form\OperationType;
 use App\Entity\Operation;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,45 +11,133 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\RadioType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Serializer\SerializerInterface;
 
 /**
 * @Route("/operation")
+* Class CompanyController
+* @package App\Controller
 */
 class OperationController extends AbstractController
 {
   /**
-  * @Route ("/new")
+  * @Route("/new", methods = {"GET","POST"})
   * @param Request $request
+  *@return \Symfony\Component\HttpFoundation\Response
   */
   public function new(Request $request){
       $operation = new Operation();
-
       $form = $this->createForm(OperationType::class, $operation);
+      $form->add('submit', SubmitType::class, [
+     'label' => 'Ajouter',
+     'attr' => ['class' => 'btn btn-default pull-right'],
+   ]);
 
       $form->handleRequest($request);
 
       if ($form->isSubmitted() && $form->isValid())
+     {
+       $em = $this->getDoctrine()->getManager();
+       $em->persist($operation);
+       $em->flush();
+       return $this->redirectToRoute('operation_list');
+     }
+
+
+      /*if($request->isMethod('POST')){
+                $this->newPost($request);
+        }*/
+
+        return $this->render('operation/new.html.twig', array(
+            'form' => $form->createView(),
+        ));
+      
+      }
+
+      /**
+     * @Route("", methods={"POST"})
+     * @param Request $request
+     */
+  public function newPost(Request $request, SerializerInterface $serializer){
+
+    $operation = new operation();
+
+    $form = $this->createForm(OperationType::class, $operation);
+    $form->handleRequest($request);
+
+    $em = $this->getDoctrine()->getManager();
+    $em->persist($operation);
+    $em->flush();
+
+    $JSON = $serializer->serrialize(
+       $company,
+       'JSON',
+       ['Groups=["light"]']
+     );
+     $response = new Response();
+     $response->setContent($JSON);
+     $response->headers->set('Content-type','application/JSON');
+     return $response;
+
+    
+
+    /*if ($form->isValid()) {
+      return $this->redirectToRoute('operation_list');
+        }*/
+
+
+    }
+
+
+    /**
+    * @Route("", methods={"PUT"})
+    * @param Request $request
+    */
+    public function editApi(){
+    }
+
+    /**
+    * @Route("", methods={"DELETE"})
+    * @param Request $request
+    */
+    public function deleteApi(){
+    }
+
+    /**
+    * @Route("/edit/{operation}")
+    * @param Request $request
+    */
+    public function edit(Request $request, Operation $operation){
+      $form = $this->createForm(OperationType::class, $operation);
+      $form->add('submit', SubmitType::class, [
+        'label' => 'Modifier',
+        'attr' => ['class' => 'btn btn-default pull-right'],
+      ]);
+
+      $form->handleRequest($request);
+      if ($form->isSubmitted() && $form->isValid())
       {
-          return $this->redirectToRoute('operation_list');
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($operation);
+        $em->flush();
+        return $this->redirectToRoute('operation_list');
       }
 
       return $this->render('operation/new.html.twig', array('form' => $form->createView()));
-  }
+    }
 
-  /**
-  * @Route ("/edit")
-  * @param Request $request
-  */
-  public function edit (Request $request){
+    /**
+    * @Route("/delete/{id}")
+    * @param Request $request
+    */
+    public function delete(Request $request){
+    }
 
-  }
-
-  /**
-  * @Route ("/delete")
-  * @param Request $request
-  */
-  public function delete (Request $request){
-
-  }
+    /**
+     * @Route("/list", name="operation_list")
+     * @param Request $request
+     */
+    public function list(Request $request){
+    }
 
 }
