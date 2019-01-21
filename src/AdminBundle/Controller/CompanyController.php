072981014137952e23b29b0411415961cb4a1174
thomas <thomas.martin6070@gmail.com>
2019-01-21 11:22:17 +0100

    modification company bundle vers api bundle

--- src/AdminBundle/Controller/CompanyController.php
+++ src/AdminBundle/Controller/CompanyController.php
@@ -43,53 +43,7 @@ class CompanyController extends AbstractController
 
       return $this->render('company/new.html.twig', array('form' => $form->createView()));
   }
-  /**
-   * @Route("", methods={"POST"})
-   * @param Request $request
-   */
-  public function newApi(Request $request, SerializerInterface $serializer)
-  {
-      $company = new Company();
-
-      $form = $this->createForm(CompanyType::class, $company);
-      $form->handleRequest($request);
-
-      $em = $this->getDoctrine()->getManager();
-      $em->persist($company);
-      $em->flush();
-
-      $JSON = $serializer->serrialize(
-        $company,
-        'JSON',
-        ['Groups=["light"]']
-      );
-      $response = new Response();
-      $response->setContent($JSON);
-      $response->headers->set('Content-type','application/JSON');
-      return $response;
-
-      // if ($form->isValid()) {
-      //
-      //   return $this->redirectToRoute('company_list');
-      // }
-  }
-
-  /**
-   * @Route("", methods={"PUT"})
-   * @param Request $request
-   */
-   public function editApi(){
-
-   }
-
-  /**
-   * @Route("", methods={"DELETE"})
-   * @param Request $request
-   */
-   public function deleteApi(){
-
-   }
-
+  
   /**
   * @Route ("/edit/{company}")
   * @param Request $request
--- src/ApiBundle/ApiBundle.php
+++ src/ApiBundle/ApiBundle.php
@@ -6,5 +6,50 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ApiBundle extends Bundle
 {
-}
+  /**
+   * @Route("", methods={"POST"})
+   * @param Request $request
+   */
+  public function newApi(Request $request, SerializerInterface $serializer)
+  {
+      $company = new Company();
 
+      $form = $this->createForm(CompanyType::class, $company);
+      $form->handleRequest($request);
+
+      $em = $this->getDoctrine()->getManager();
+      $em->persist($company);
+      $em->flush();
+
+      $JSON = $serializer->serrialize(
+        $company,
+        'JSON',
+        ['Groups=["light"]']
+      );
+      $response = new Response();
+      $response->setContent($JSON);
+      $response->headers->set('Content-type','application/JSON');
+      return $response;
+
+      // if ($form->isValid()) {
+      //
+      //   return $this->redirectToRoute('company_list');
+      // }
+  }
+
+  /**
+   * @Route("", methods={"PUT"})
+   * @param Request $request
+   */
+   public function editApi(){
+
+   }
+
+  /**
+   * @Route("", methods={"DELETE"})
+   * @param Request $request
+   */
+   public function deleteApi(){
+
+   }
+}
