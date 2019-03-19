a1e070ca6553b65964ad899c1789634d806d7293
SofianeAguero <azi.sofiane@hotmail.fr>
2019-03-19 17:13:54 +0100

    ajout de la page de connexion et de son integration

--- src/AdminBundle/Controller/SecurityController.php
+++ src/AdminBundle/Controller/SecurityController.php
@@ -1,18 +1,19 @@
 <?php
 
-namespace App\Controller;
+namespace App\AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Doctrine\Common\Persistence\ObjectManager;
 
 class SecurityController extends AbstractController
 {
     /**
      * @Route("/login", name="app_login")
      */
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, ObjectManager $manager): Response
     {
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
@@ -20,5 +21,12 @@ class SecurityController extends AbstractController
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout()
+    {
     }
 }
